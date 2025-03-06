1499da7d7f82e63e7cb9e07e28cdb384df3f4abc
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-05 22:58:16 -0300

    Alterado o ItensVenda para uma lista de itemVenda

--- src/Models/Venda.java
+++ src/Models/Venda.java
@@ -1,22 +1,18 @@
 package Models;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Venda {
-    
+
     private int id;
     private Cliente cliente;
     private Funcionario vendedor;
     private LocalDate data;
     private Pagamento pagamento;
-    private ItemVenda itensVenda;
+    private List<ItemVenda> itensVenda;
 
-     public Venda(int id, 
-        Cliente cliente, 
-        Funcionario vendedor, 
-        LocalDate data, 
-        Pagamento pagamento, 
-        ItemVenda itensVenda) {
+    public Venda(int id, Cliente cliente, Funcionario vendedor, LocalDate data, Pagamento pagamento, List<ItemVenda> itensVenda) {
         this.id = id;
         this.cliente = cliente;
         this.vendedor = vendedor;
@@ -28,55 +24,77 @@ public class Venda {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public Cliente getCliente() {
         return cliente;
     }
+
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
+
     public Funcionario getVendedor() {
         return vendedor;
     }
+
     public void setVendedor(Funcionario vendedor) {
         this.vendedor = vendedor;
     }
+
     public LocalDate getData() {
         return data;
     }
+
     public void setData(LocalDate data) {
         this.data = data;
     }
+
     public Pagamento getPagamento() {
         return pagamento;
     }
+
     public void setPagamento(Pagamento pagamento) {
         this.pagamento = pagamento;
     }
-    public ItemVenda getItensVenda() {
+
+    public List<ItemVenda> getItensVenda() {
         return itensVenda;
     }
-    public void setItensVenda(ItemVenda itensVenda) {
-        this.itensVenda = itensVenda;
+
+    public void setItensVenda(ItemVenda itemVenda) {
+        this.itensVenda.add(itemVenda);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("id da venda: ").append(id).append("\n")
-                .append("DADOS DO CLIENTE: \n").append(cliente).append("\n")
+        sb.append("id da venda: ")
+                .append(id)
                 .append("\n")
-                .append("DADOS DO VENDEDOR: \n").append(vendedor).append("\n")
-                .append("\n")
-                .append("DATA DA VENDA: \n").append(data).append("\n")
-                .append("\n")
-                .append("DADOS DO PAGEMENTO: \n").append(pagamento).append("\n")
-                .append("\n")
-                .append("ITENS DA VENDA: \n").append(itensVenda).append("\n")
-                .append("\n")
-                .append("--------------------------------------------------");
+                .append("DADOS DO CLIENTE: \n")
+                .append(cliente)
+                .append("\n\n")
+                .append("DADOS DO VENDEDOR: \n")
+                .append(vendedor)
+                .append("\n\n")
+                .append("DATA DA VENDA: \n")
+                .append(data)
+                .append("\n\n")
+                .append("DADOS DO PAGEMENTO: \n")
+                .append(pagamento)
+                .append("\n\n")
+                .append("ITENS DA VENDA: \n");
+
+        for (ItemVenda itemVenda : itensVenda) {
+            sb.append(itemVenda).append("\n\n");
+        }
+
+        sb.append("--------------------------------------------------");
+
         return sb.toString();
     }
 }
