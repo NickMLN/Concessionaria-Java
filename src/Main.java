afe3aee80046d87861e09923a374b4727535f094
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-05 22:59:43 -0300

    Ajustado a formatação e a tipagem de itens da venda

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,6 @@ import Models.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
 public class Main {
